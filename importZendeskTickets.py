e58cedaacea9750d86193c2d25d68d9c5ccf0ced
asadisaghar <asadisaghar@gmail.com>
2019-02-21 08:55:25 +0100

    init

---
+++ importZendeskTickets.py
@@ -0,0 +1,80 @@
+import requests
+import json
+from credentials import credentials
+import os
+
+def get_ticket(ticketid, cred=credentials):
+    url = '{}/tickets/{}.json'.format(cred['url'], ticketid)
+    response = requests.get(url, auth=(cred['user'], cred['pwd']))
+    return response.json()
+    
+def get_all_orgs(cred=credentials):
+    url = '{}/organizations.json'.format(cred['url'])
+    response = requests.get(url, auth=(cred['user'], cred['pwd']))
+    return response.json()
+
+def get_org_tickets(orgid, url='', cred=credentials):
+    if not url:
+        url = '{}/organizations/{}/tickets.json'.format(cred['url'], orgid)
+    response = requests.get(url, auth=(cred['user'], cred['pwd']))
+    return response.json()
+
+def get_ticket_comments(ticketid, url='', cred=credentials):
+    if not url:
+        url = '{}/tickets/{}/comments.json'.format(cred['url'], ticketid)
+    response = requests.get(url, auth=(cred['user'], cred['pwd']))
+    return response.json()
+    
+def get_all_tickets(tickets_path='tickets/', cred=credentials):
+    if not os.path.exists(tickets_path):
+        os.makedirs(tickets_path)
+
+    all_orgs = get_all_orgs()
+    orgs = [x['id'] for x in all_orgs['organizations']]
+    for orgid in orgs:
+        org_tickets = '{}.json'.format(orgid)
+        print(orgid)
+        tickets = []
+        resp = get_org_tickets(orgid)
+        #fix pagination!
+        tickets.append(resp['tickets'])
+        while resp['next_page']:
+            print(resp['next_page'])
+            resp = get_org_tickets(orgid, resp['next_page'])
+            tickets.append(resp['tickets'])
+            
+        with open(os.path.join(tickets_path, org_tickets), 'w') as fout:
+            json.dump(tickets, fout)
+            fout.write("\n")
+    print('All tickets are exported!')
+
+def get_all_comments(comments_path='comments/', tickets_path='tickets/', cred=credentials):
+    for filename in os.listdir(tickets_path):
+        orgid = filename.split('.json')[0]
+        orgpath = os.path.join(comments_path, orgid)
+        print(orgpath)
+        if os.path.exists(orgpath):
+            pass
+        else:
+            os.makedirs(orgpath)
+            with open(os.path.join(tickets_path, filename)) as f:
+                tickets = json.load(f)
+            ticket_ids = [ticket['id'] for ticket in tickets[0]]
+            for ticketid in ticket_ids:
+                print(ticketid)
+                ticket_comments = '{}.json'.format(ticketid)
+                comments = []
+                resp = get_ticket_comments(ticketid)
+                comments.append(resp['comments'])
+                while resp['next_page']:
+                    print(resp['next_page'])
+                    resp = get_ticket_comments(ticketid, resp['next_page'])
+                    comments.append(resp['tickets'])
+                with open(os.path.join(orgpath, ticket_comments), 'w') as fout:
+                    json.dump(comments, fout)
+                    fout.write("\n")
+            print('All comments are exported!')
+
+            
+get_all_tickets()
+get_all_comments()
